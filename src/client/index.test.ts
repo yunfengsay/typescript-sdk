9774e2fdfe74b44490fe31bc8f36e59d9cccfbd0
Justin Spahr-Summers <justin@anthropic.com>
2024-11-04 21:10:35 +0000

    Disable unused vars lint

--- src/client/index.test.ts
+++ src/client/index.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Client } from "./index.js";
 import { z } from "zod";
 import { RequestSchema, NotificationSchema, ResultSchema } from "../types.js";
--- src/server/index.test.ts
+++ src/server/index.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Server } from "./index.js";
 import { z } from "zod";
 import { RequestSchema, NotificationSchema, ResultSchema } from "../types.js";
