a6e78b742692a0a0349ab7c427fa0a307fd05165
Justin Spahr-Summers <justin@anthropic.com>
2025-02-18 09:52:45 +0000

    Add handler for auth server metadata

---
+++ src/server/auth/handlers/metadata.ts
@@ -0,0 +1,17 @@
+import express, { RequestHandler } from "express";
+import { OAuthMetadata } from "../../../shared/auth.js";
+import cors from 'cors';
+
+export function metadataHandler(metadata: OAuthMetadata): RequestHandler {
+  // Nested router so we can configure middleware and restrict HTTP method
+  const router = express.Router();
+
+  // Configure CORS to allow any origin, to make accessible to web-based MCP clients
+  router.use(cors());
+
+  router.get("/", (req, res) => {
+    res.status(200).json(metadata);
+  });
+
+  return router;
+}
